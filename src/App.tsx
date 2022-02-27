7340b31ec79d5a2e9dde1a6efdb6869e6408a339
Bob Jackson <yousong.wang@highsoft.ltd>
2022-02-27 23:12:50 +0800

    use Playground

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,9 @@ function App() {
     return (
         <div className={styles.app}>
             <Logo/>
-            <Main/>
+            <main className={styles.content}>
+                <Main/>
+            </main>
             <Footer/>
         </div>
     );
